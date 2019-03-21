4c194ac60a97c24d5bf08eaa95632a00077bda41
Ayrton-Taede BeFoRe <ayrton.taede@gmail.com>
2019-03-21 13:36:03 +0100

    Changed logic and added a comment

--- Laadarm/ssd_example.cpp
+++ Laadarm/ssd_example.cpp
@@ -11,7 +11,7 @@ uint8_t n = 0;
 uint8_t ssdPins[] = {4, 5, 6, 7, 8, 9, 10};
 SSD seg7(3, 2, ssdPins);
 Button button(11);
-LS ls(5, 50);
+LS ls(5, 240);
 
 void stage()
 {
@@ -19,8 +19,9 @@ void stage()
   bool stillLoaded = true;
   while (true)
   {
-    if (ls.isDark())
+    if (ls.isDark() && stillLoaded)
     {
+      // TODO: Code voor afgeven van lading
       stillLoaded = false;
     }
 
