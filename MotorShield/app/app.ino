abae964aeaffdf126a195fb3424e7acb9ebb70cf
Timon Post <timonpost@hotmail.nl>
2019-03-24 21:54:50 +0100

    added code back

--- MotorShield/app/app.ino
+++ MotorShield/app/app.ino
@@ -41,56 +41,56 @@ void loop()
   // Getting sensorValues
   lineDetector.gatherSensorResults(sensorValues);
 
-  // if (lineDetector.leftSideSensorsEnabled(sensorValues))
-  // {
-  //   turnRight();
-  //   lastDetection = Direction::left;
-  // }
-  // else if (lineDetector.rightSideSensorsEnabled(sensorValues))
-  // {
-  //   turnLeft();
-  //   lastDetection = Direction::right;
-  // }
-  // else if (lineDetector.middleSensorsEnabled(sensorValues))
-  // {
+  if (lineDetector.leftSideSensorsEnabled(sensorValues))
+  {
+    turnRight();
+    lastDetection = Direction::left;
+  }
+  else if (lineDetector.rightSideSensorsEnabled(sensorValues))
+  {
+    turnLeft();
+    lastDetection = Direction::right;
+  }
+  else if (lineDetector.middleSensorsEnabled(sensorValues))
+  {
 
-  //   // if turning and we are on the line, we should disable one weel from spinning reverse.
-  //   // if (lastDetection == Direction::left)
-  //   //   car.leftMotor.reverse(false);
-  //   // else if (lastDetection == Direction::right)
-  //   //   car.leftMotor.reverse(false);
+    // if turning and we are on the line, we should disable one weel from spinning reverse.
+    // if (lastDetection == Direction::left)
+    //   car.leftMotor.reverse(false);
+    // else if (lastDetection == Direction::right)
+    //   car.leftMotor.reverse(false);
 
-  //   car.constant(Speed::Average);
-  // }
-  // else if (lineDetector.noSensorsDetected(sensorValues))
-  // {
-  //   if (lastDetection == Direction::left)
-  //   {
-  //     turnRight();
-  //   }
-  //   else if (lastDetection == Direction::right)
-  //   {
-  //     turnLeft();
-  //   }
-  //   else
-  //   {
-  //   }
-  // }
-  // else
-  // {
-  //   car.stop();
-  // }
-  Serial.print(sensorValues[2]);
-    
-  Serial.println();
+    car.constant(Speed::Average);
+  }
+  else if (lineDetector.noSensorsDetected(sensorValues))
+  {
+    if (lastDetection == Direction::left)
+    {
+      turnRight();
+    }
+    else if (lastDetection == Direction::right)
+    {
+      turnLeft();
+    }
+    else
+    {
+    }
+  }
+  else
+  {
+    car.stop();
+  }
+  Serial.println(sensorValues[2]);
 }
 
-void turnLeft() {
+void turnLeft()
+{
   car.rightMotor.setSpeed(Speed::Average);
   car.leftMotor.setSpeed(Speed::Stationair);
 }
 
-void turnRight() {
-    car.rightMotor.setSpeed(Speed::Stationair);
-    car.leftMotor.setSpeed(Speed::Average);
+void turnRight()
+{
+  car.rightMotor.setSpeed(Speed::Stationair);
+  car.leftMotor.setSpeed(Speed::Average);
 }
